f93af5e4c8c92b60d59e97b5b699edfea05f26ea
Milos Rasuo <raske1980@gmail.com>
2025-01-25 00:04:35 +0100

    football api integration

---
+++ Code/AISoccerAPI/API/FootballAPI/Leagues/FootbalApiFixturesResponse.cs
@@ -0,0 +1,248 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class Parameters
+{
+    [JsonProperty("league")]
+    public string League { get; set; }
+
+    [JsonProperty("season")]
+    public string Season { get; set; }
+}
+
+public class APIFixturePaging
+{
+    [JsonProperty("current")]
+    public int Current { get; set; }
+
+    [JsonProperty("total")]
+    public int Total { get; set; }
+}
+
+public class Periods
+{
+    [JsonProperty("first")]
+    public long First { get; set; }
+
+    [JsonProperty("second")]
+    public long Second { get; set; }
+}
+
+public class Venue
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("city")]
+    public string City { get; set; }
+}
+
+public class Status
+{
+    [JsonProperty("long")]
+    public string Long { get; set; }
+
+    [JsonProperty("short")]
+    public string Short { get; set; }
+
+    [JsonProperty("elapsed")]
+    public int Elapsed { get; set; }
+
+    [JsonProperty("extra")]
+    public object Extra { get; set; }
+}
+
+public class Fixture
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("referee")]
+    public string Referee { get; set; }
+
+    [JsonProperty("timezone")]
+    public string Timezone { get; set; }
+
+    [JsonProperty("date")]
+    public string Date { get; set; }
+
+    [JsonProperty("timestamp")]
+    public long Timestamp { get; set; }
+
+    [JsonProperty("periods")]
+    public Periods Periods { get; set; }
+
+    [JsonProperty("venue")]
+    public Venue Venue { get; set; }
+
+    [JsonProperty("status")]
+    public Status Status { get; set; }
+}
+
+public class APIFixtureLeague
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("country")]
+    public string Country { get; set; }
+
+    [JsonProperty("logo")]
+    public string Logo { get; set; }
+
+    [JsonProperty("flag")]
+    public string Flag { get; set; }
+
+    [JsonProperty("season")]
+    public int Season { get; set; }
+
+    [JsonProperty("round")]
+    public string Round { get; set; }
+
+    [JsonProperty("standings")]
+    public bool Standings { get; set; }
+}
+
+public class Home
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("logo")]
+    public string Logo { get; set; }
+
+    [JsonProperty("winner")]
+    public bool? Winner { get; set; }
+}
+
+public class Away
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("logo")]
+    public string Logo { get; set; }
+
+    [JsonProperty("winner")]
+    public bool? Winner { get; set; }
+}
+
+public class Teams
+{
+    [JsonProperty("home")]
+    public Home Home { get; set; }
+
+    [JsonProperty("away")]
+    public Away Away { get; set; }
+}
+
+public class Goals
+{
+    [JsonProperty("home")]
+    public int Home { get; set; }
+
+    [JsonProperty("away")]
+    public int Away { get; set; }
+}
+
+public class Halftime
+{
+    [JsonProperty("home")]
+    public int Home { get; set; }
+
+    [JsonProperty("away")]
+    public int Away { get; set; }
+}
+
+public class Fulltime
+{
+    [JsonProperty("home")]
+    public int Home { get; set; }
+
+    [JsonProperty("away")]
+    public int Away { get; set; }
+}
+
+public class Extratime
+{
+    [JsonProperty("home")]
+    public object Home { get; set; }
+
+    [JsonProperty("away")]
+    public object Away { get; set; }
+}
+
+public class Penalty
+{
+    [JsonProperty("home")]
+    public object Home { get; set; }
+
+    [JsonProperty("away")]
+    public object Away { get; set; }
+}
+
+public class Score
+{
+    [JsonProperty("halftime")]
+    public Halftime Halftime { get; set; }
+
+    [JsonProperty("fulltime")]
+    public Fulltime Fulltime { get; set; }
+
+    [JsonProperty("extratime")]
+    public Extratime Extratime { get; set; }
+
+    [JsonProperty("penalty")]
+    public Penalty Penalty { get; set; }
+}
+
+public class APIFixtureResponse
+{
+    [JsonProperty("fixture")]
+    public Fixture Fixture { get; set; }
+
+    [JsonProperty("league")]
+    public APIFixtureLeague League { get; set; }
+
+    [JsonProperty("teams")]
+    public Teams Teams { get; set; }
+
+    [JsonProperty("goals")]
+    public Goals Goals { get; set; }
+
+    [JsonProperty("score")]
+    public Score Score { get; set; }
+}
+
+public class FootbalAPIFixtureResponse
+{
+    [JsonProperty("get")]
+    public string Get { get; set; }
+
+    [JsonProperty("parameters")]
+    public Parameters Parameters { get; set; }
+
+    [JsonProperty("errors")]
+    public List<object> Errors { get; set; }
+
+    [JsonProperty("results")]
+    public int Results { get; set; }
+
+    [JsonProperty("paging")]
+    public APIFixturePaging Paging { get; set; }
+
+    [JsonProperty("response")]
+    public List<APIFixtureResponse> Response { get; set; }
+}
--- Code/AISoccerAPI/Program.cs
+++ Code/AISoccerAPI/Program.cs
@@ -70,14 +70,56 @@ try
 
     #region New Sources
 
-    //for new matches, Football API
+    //for new matches, Football API, get all leagues
     var client = new RestClient("https://v3.football.api-sports.io/leagues");
     var request = new RestRequest();
     request.AddHeader("x-rapidapi-key", footballAPIKey);
     request.AddHeader("x-rapidapi-host", footballAPIUrl);
     RestResponse response = client.Execute(request);
     var apiLeagueDetailResponses = JsonConvert.DeserializeObject<FootballAPILeaguesResponse>(response.Content);
-    var apiFootballLeagueIds = apiLeagueDetailResponses.Response.FindAll(x=>x.League.Type == "League").ToList().Select(x=>x.League.Id).ToList();  
+    var apiFootballLeagueIds = apiLeagueDetailResponses.Response.FindAll(x => x.League.Type == "League").ToList().Select(x => x.League.Id).ToList();
+    var apiFootballLeagues = apiLeagueDetailResponses.Response.FindAll(x => x.League.Type == "League").ToList();
+
+    //get leagues by country
+    var client2 = new RestClient("https://v3.football.api-sports.io/leagues?code=FR");
+    var request2 = new RestRequest();
+    request2.AddHeader("x-rapidapi-key", footballAPIKey);
+    request2.AddHeader("x-rapidapi-host", footballAPIUrl);
+    RestResponse response2 = client2.Execute(request2);   
+
+    //get season info by id
+    var client1 = new RestClient("https://v3.football.api-sports.io/leagues?season=2021");
+    var request1 = new RestRequest();
+    request1.AddHeader("x-rapidapi-key", footballAPIKey);
+    request1.AddHeader("x-rapidapi-host", footballAPIUrl);
+    RestResponse response1 = client1.Execute(request1);    
+
+    //get match info by season id and match id, https://v3.football.api-sports.io/leagues?season=2019&id=39");
+    var client3 = new RestClient("https://v3.football.api-sports.io/leagues?season=2021&id=61");
+    var request3 = new RestRequest();
+    request3.AddHeader("x-rapidapi-key", footballAPIKey);
+    request3.AddHeader("x-rapidapi-host", footballAPIUrl);
+    RestResponse response3 = client3.Execute(request3);    
+
+    //get fixtures by season id, https://v3.football.api-sports.io/fixtures?league=39&season=2023
+    var client4 = new RestClient("https://v3.football.api-sports.io/fixtures?league=39&season=2023");
+    var request4 = new RestRequest();
+    request4.AddHeader("x-rapidapi-key", footballAPIKey);
+    request4.AddHeader("x-rapidapi-host", footballAPIUrl);
+    RestResponse response4 = client4.Execute(request4);    
+    var fixtureResponse = JsonConvert.DeserializeObject<FootbalAPIFixtureResponse>(response4.Content);
+    
+    //work flow will be like this, first call I am going to get all the leagues,
+    //I must create exclusion list of leagues that I already have,
+    //I will iterate through league's seasons by id and get fixtures for that season id,
+    //I am going to create list of matches by league by iterating through all league's seasons,
+    //and I am going to use that list to save it as MatchFeatures.csv file by using season id inside league folder
+    //and later I am going to concanate all csvs with SoccerApi csv and train the model at the end,
+    //for beginning I am going to create up to 90 csvs per day because of the free plan limitations (100 requests per day)
+    //so there is about 700+ leagues and I can get for free 3 seasons per league (2021,2022,2023) so it will take about 3.5 weeks to get all the data,
+    //but I will train model at the end of the day with newly aquired data,
+    //so I will need to create mechanism for csv concatenation after I create mechanism for Football API csv serialiazation as MatchFeature objects
+    //csv serialization must include yyyyMMdd info so I will work only with newest csv files like I created for model save/load
 
     #endregion
 }
