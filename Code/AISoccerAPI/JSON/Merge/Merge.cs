47a38f76aec9368a5897648ee9646fba9f8fd456
Milos Rasuo <raske1980@gmail.com>
2025-02-20 17:49:33 +0100

    code refactor

--- Code/AISoccerAPI/JSON/Merge/Merge.cs
+++ Code/AISoccerAPI/JSON/Merge/Merge.cs
@@ -16,8 +16,8 @@ namespace AISoccerAPI.JSON.Merge
     {
         #region Properties
 
-        public const int OpenDataRandomLimit = 3000;
-        public const int FootballJSONRandomLimit = 150000;
+        private const int OpenDataRandomLimit = 3000;
+        private const int FootballJSONRandomLimit = 150000;
 
         #endregion
 
@@ -33,6 +33,8 @@ namespace AISoccerAPI.JSON.Merge
 
         #region Public Methods
 
+        #region Merge/Transform
+
         public List<MatchFeatures> MergeAll(Dictionary<string, List<(int competitionId, List<OpenData.Data.Match> matches)>> openDataJSON,
             Dictionary<string, List<(string competition, List<AISoccerAPI.JSON.FootballJSON.Data.MatchExt> matches)>> footballJSON,
             AppConfig appConfig)
@@ -41,32 +43,19 @@ namespace AISoccerAPI.JSON.Merge
             return GetMatchFeatures(jsonMatches);
         }
 
-        public List<JSONMatch> MergeJSONData(Dictionary<string, List<(int competitionId, List<OpenData.Data.Match> matches)>> openDataJSON,
-            Dictionary<string, List<(string competition, List<AISoccerAPI.JSON.FootballJSON.Data.MatchExt> matches)>> footballJSON,
-            AppConfig appConfig)
+        public List<JSONMatch> TransformOpenDataMatches(Dictionary<string, List<(int competitionId, List<OpenData.Data.Match> matches)>> openDataJSON,
+            AppConfig appConfig,
+            List<int> openDataMatchIds)
         {
-            //this method needs to normalize data from all sources, remove duplicates
             List<JSONMatch> toReturn = new List<JSONMatch>();
-
-            Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>> leagueMappings = 
-                new Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>>();
-            leagueMappings.Add("Germany", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "1. Bundesliga") });
-            leagueMappings.Add("Spain", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "La Liga") });
-            leagueMappings.Add("England", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Premier League") });
-            leagueMappings.Add("Argentina", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Liga Profesional") });
-            leagueMappings.Add("France", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Ligue 1") });
-            leagueMappings.Add("Italy", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Serie A") });
-            List<string> leagueNames = new List<string> { "1. Bundesliga", "La Liga", "Premier League", "Liga Professional", "Ligue 1", "Serie A" };
-
             //iterate through open data matches
-            var openDataCompetitions = new OpenDataExtract().GetCompetitions(appConfig);
-            List<int> openDataMatchIds = new List<int>();
-            foreach(var keyValuePair in openDataJSON)
+            var openDataCompetitions = new OpenDataExtract().GetCompetitions(appConfig);            
+            foreach (var keyValuePair in openDataJSON)
             {
-                foreach(var item in keyValuePair.Value)
+                foreach (var item in keyValuePair.Value)
                 {
                     var competition = openDataCompetitions.FirstOrDefault(x => x.CompetitionId == item.competitionId);
-                    foreach(var match in item.matches)
+                    foreach (var match in item.matches)
                     {
                         var matchDate = DateTime.MinValue;
                         DateTime.TryParse(match.match_date, out matchDate);
@@ -74,8 +63,9 @@ namespace AISoccerAPI.JSON.Merge
                         while (openDataMatchIds.Contains(matchId))
                             matchId = new Random().Next(1, OpenDataRandomLimit);
                         openDataMatchIds.Add(matchId);
-                        
-                        toReturn.Add(new JSONMatch { 
+
+                        toReturn.Add(new JSONMatch
+                        {
                             AwayTeam = match.away_team.away_team_name,
                             AwayTeamGoals = match.away_score,
                             HomeTeam = match.home_team.home_team_name,
@@ -87,18 +77,28 @@ namespace AISoccerAPI.JSON.Merge
                             MatchId = matchId
                         });
                     }
-                }                
+                }
             }
 
+            return toReturn;
+        }
+
+        public List<JSONMatch> TransformFootballJSONMatches(Dictionary<string, List<(string competition, List<AISoccerAPI.JSON.FootballJSON.Data.MatchExt> matches)>> footballJSON,
+            Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>> leagueMappings,
+            List<string> leagueNames,
+            List<int> openDataMatchIds)
+        {
+            List<JSONMatch> toReturn = new List<JSONMatch>();
+
             //iterate through FootballJSON matches
-            List<int> footballJSONMatchIds = new List<int>();            
-            foreach(var keyValuePair in footballJSON)
+            List<int> footballJSONMatchIds = new List<int>();
+            foreach (var keyValuePair in footballJSON)
             {
                 var country = keyValuePair.Key;
-                
+
                 foreach (var item in keyValuePair.Value)
                 {
-                    foreach(var match in item.matches)
+                    foreach (var match in item.matches)
                     {
                         var matchDate = DateTime.MinValue;
                         DateTime.TryParse(match.date, out matchDate);
@@ -106,8 +106,8 @@ namespace AISoccerAPI.JSON.Merge
                         while (openDataMatchIds.Contains(matchId))
                             matchId = new Random().Next(OpenDataRandomLimit + 1, FootballJSONRandomLimit);
                         openDataMatchIds.Add(matchId);
-                        if(toReturn.FirstOrDefault(x=>x.Country == country && 
-                                                        x.HomeTeam == match.team1 && 
+                        if (toReturn.FirstOrDefault(x => x.Country == country &&
+                                                        x.HomeTeam == match.team1 &&
                                                         x.AwayTeam == match.team2) == null &&
                                                         match.score != null && match.score.ft != null)
                         {
@@ -120,7 +120,7 @@ namespace AISoccerAPI.JSON.Merge
                             foreach (var competition in leagueNames)
                                 seasonName = seasonName.Replace(competition, string.Empty).Trim();
 
-                            if(string.IsNullOrEmpty(mappedCompetitionName))
+                            if (string.IsNullOrEmpty(mappedCompetitionName))
                             {
                                 var seasonNameList = seasonName.Split(new char[1] { ' ' }).ToList();
                                 seasonName = seasonNameList[seasonNameList.Count - 1];
@@ -142,12 +142,33 @@ namespace AISoccerAPI.JSON.Merge
                                 Season = seasonName
                             });
                         }
-                    }                    
+                    }
                 }
             }
+
+            return toReturn;
+        }
+
+        public List<JSONMatch> MergeJSONData(Dictionary<string, List<(int competitionId, List<OpenData.Data.Match> matches)>> openDataJSON,
+            Dictionary<string, List<(string competition, List<AISoccerAPI.JSON.FootballJSON.Data.MatchExt> matches)>> footballJSON,
+            AppConfig appConfig)
+        {
+            //this method needs to normalize data from all sources, remove duplicates
+            List<JSONMatch> toReturn = new List<JSONMatch>();
+
+            var leagueMappings = GetLeagueMappings();
+            var leagueNames = GetLeagueNames();
+            List<int> openDataMatchIds = new List<int>();
+
+            toReturn.AddRange(TransformOpenDataMatches(openDataJSON, appConfig, openDataMatchIds));
+            toReturn.AddRange(TransformFootballJSONMatches(footballJSON, leagueMappings, leagueNames, openDataMatchIds));            
            
             return toReturn;
         }
+
+        #endregion
+
+        #region Construct Match Features
 
         public List<MatchFeatures> GetMatchFeatures(List<JSONMatch> matches)
         {
@@ -194,7 +215,28 @@ namespace AISoccerAPI.JSON.Merge
 
         #endregion
 
+        #endregion
+
         #region Private Methods
+
+        private Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>> GetLeagueMappings()
+        {
+            Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>> leagueMappings =
+                new Dictionary<string, List<(string leagueOpenData, string leagueFootballJSON)>>();
+            leagueMappings.Add("Germany", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "1. Bundesliga") });
+            leagueMappings.Add("Spain", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "La Liga") });
+            leagueMappings.Add("England", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Premier League") });
+            leagueMappings.Add("Argentina", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Liga Profesional") });
+            leagueMappings.Add("France", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Ligue 1") });
+            leagueMappings.Add("Italy", new List<(string leagueOpenData, string leagueFootballJSON)> { ("1", "Serie A") });
+            return leagueMappings;
+        }
+
+        private List<string> GetLeagueNames()
+        {
+            List<string> leagueNames = new List<string> { "1. Bundesliga", "La Liga", "Premier League", "Liga Professional", "Ligue 1", "Serie A" };
+            return leagueNames;
+        }
 
         #endregion
 
